9daa05899e78c4a5306e28ee5db408afb3790892
Daniel Rodriguez <danielr2397@gmail.com>
2022-02-22 16:21:23 -0500

    Create navbar section & import icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
+import { Avocado, Basket } from "./SVGIcons";
 
 const Navbar = () => {
 	return (
-		<nav>
-			<Link href="/">
-				<a>home</a>
-			</Link>
-			<Link href="/about">
-				<a>About</a>
-			</Link>
-			<Link href="/product/123">
-				<a>Product</a>
-			</Link>
-		</nav>
+		<header>
+			<nav className="nav">
+				<section className="nav-section">
+					<Avocado size="45" />
+					<Link href="/">
+						<a className="nav-anchor">Products</a>
+					</Link>
+				</section>
+				<section className="nav-section">
+					<Basket size="45" />
+					<Link href="/shopping/123">
+						<a className="nav-anchor">Shopping cart</a>
+					</Link>
+				</section>
+			</nav>
+		</header>
 	);
 };
 
